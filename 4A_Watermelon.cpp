eb9f29f96a95e50ae6d6636e0fea96c420079b40
AndresEngineer <andrespengineer@gmail.com>
2015-07-29 03:59:12 -0400

    Create 4A_Watermelon.cpp

---
+++ 4A_Watermelon.cpp
@@ -0,0 +1,29 @@
+// Codeforces_Andrespengineer
+/* List of solved problems  
+ * 
+ * Andrés Sierra
+ * Instituto Tecnológico de Santo Domingo (INTEC)
+ *
+ */ 
+ 
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int w;
+    
+    cin >> w;
+    
+    if(w <= 2){
+        cout << "NO" << endl;
+    }
+    else if(w%2 == 0){
+        cout << "YES" << endl;
+  
+    }
+    else{
+        cout << "NO" << endl;
+    }    
+    
+}
