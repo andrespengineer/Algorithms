703b5e2dce86419588df70f608341c8a38c49b3d
AndresEngineer <andrespengineer@gmail.com>
2015-07-29 04:08:38 -0400

    Create 50A_DominoPiling.cpp

---
+++ 50A_DominoPiling.cpp
@@ -0,0 +1,22 @@
+// Codeforces_Andrespengineer
+/* List of solved problems  
+ * 
+ * Andrés Sierra
+ * Instituto Tecnológico de Santo Domingo (INTEC)
+ *
+ */ 
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    
+    int m, n;
+    
+    cin >> m >> n;
+    
+    int p = m*n;
+    
+    cout << p/2 << endl;
+}
